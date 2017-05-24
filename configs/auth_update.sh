8b0b9936b8b38bd1d4d4ec4de06019f3d022c89c
catalinpan <catalin.gf@gmail.com>
2017-05-24 22:51:37 +0100

    upgraded to the latest stable versions of awscli and nginx

--- configs/auth_update.sh
+++ configs/auth_update.sh
@@ -4,7 +4,7 @@ nx_conf=/etc/nginx/nginx.conf
 
 # update the auth token
 auth=$(grep  X-Forwarded-User ${nx_conf} | awk '{print $4}'| uniq|tr -d "\n\r")
-token=$(aws ecr get-login | awk '{print $6}')
+token=$(aws ecr get-login --no-include-email | awk '{print $6}')
 auth_n=$(echo AWS:${token}  | base64 |tr -d "[:space:]")
 
 sed -i "s|${auth%??}|${auth_n}|g" ${nx_conf}
