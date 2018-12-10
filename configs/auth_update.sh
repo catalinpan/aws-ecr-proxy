4003e90866608948ea2a2a6d13d88a9d156dd4a0
catalinpan <catalin.gf@gmail.com>
2018-12-10 01:16:24 +0000

    fix for https://github.com/catalinpan/aws-ecr-proxy/issues/7, registry ID not being used to renew token

--- configs/auth_update.sh
+++ configs/auth_update.sh
@@ -5,10 +5,10 @@ nx_conf=/etc/nginx/nginx.conf
 # update the auth token
 auth=$(grep  X-Forwarded-User ${nx_conf} | awk '{print $4}'| uniq|tr -d "\n\r")
 if [ "$REGISTRY_ID" = "" ]
-then 
+then
     token=$(aws ecr get-login --no-include-email | awk '{print $6}')
 else
-    aws_cli_exec=$(aws ecr get-login --no-include-email --registry-ids $REGISTRY_ID)
+    token=$(aws ecr get-login --no-include-email --registry-ids $REGISTRY_ID | awk '{print $6}')
 fi
 auth_n=$(echo AWS:${token}  | base64 |tr -d "[:space:]")
 
